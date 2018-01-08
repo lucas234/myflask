19c11c911e67b27df3e6cea04035e9f46c2e0dd3
lucas234 <ly_liubo@163.com>
2018-01-08 16:01:09 +0800

    test

--- myapp/function/API/params.py
+++ myapp/function/API/params.py
@@ -1,7 +1,7 @@
 # _*_ coding=utf-8 _*_
 
 from urlparse import urljoin
-host = "http://192.168.40.10"
+host = "http://XXXXXX"
 url_img = urljoin(host, "/register/captcha")
 url_sms = urljoin(host, "/register/sms")
 url_sub = urljoin(host, "/register/submit")
