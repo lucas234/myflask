1404102d8e9d74c5e25f7758a34884d7169d4652
lucas234 <ly_liubo@163.com>
2018-01-08 16:02:03 +0800

    test

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 # 从数据库生成model
 # sqlacodegen --noviews --noconstraints --noindexes --outfile H:\study\FLASK\testmodels.py mysql://root:123456@192.168.40.192:3306/testtools
 
-SQLALCHEMY_DATABASE_URI = "mysql://root:123456@192.168.40.192:3306/testtools?charset=utf8"  # 'mysql+pymysql://root:xxxxx@localhost:3306/test?charset=utf8'
+SQLALCHEMY_DATABASE_URI = "mysql://root:123456@XXXXXX:3306/testtools?charset=utf8"  # 'mysql+pymysql://root:xxxxx@localhost:3306/test?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
