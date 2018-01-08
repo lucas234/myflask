65cc9f7cff0cfce5e63ffae1dfd495360cb9bc2f
lucas234 <ly_liubo@163.com>
2018-01-08 16:04:49 +0800

    test

---
+++ myapp/function/config.py
@@ -0,0 +1,55 @@
+# _*_ coding=utf-8 _*_
+__author__ = 'D054'
+
+old_huolicai = {
+    "host": 'XXXX',
+    "port": 22,
+    "user": "XXXXXX",
+    "key": r"C:\Users\saas\Desktop\id_rsa_2048.id_rsa_2048",  # 秘钥
+    "password": "XXXXX",
+}
+
+ultron_huolicai = {
+    "host": 'XXXXX',
+    "port": 22,
+    "user": "XXXXX",
+    "key": r"C:\Users\saas\Desktop\id_rsa_2048.id_rsa_2048",  # 秘钥
+    "password": "XXXXXX",
+}
+
+
+mysql_config = {
+    "host": 'XXXXXX',
+    "port": 3306,
+    "user": "root",
+    # "key": r"C:\Users\saas\Desktop\id_rsa_2048.id_rsa_2048",  # 秘钥
+    "passwd": "XXXXX",
+    "db": "XXXX",
+}
+
+redis_config = {
+    "host": "XXXX",
+    "port": "6379",
+    "password": "XXXXX",
+    "db": "0",
+}
+
+
+mysql_config_192 = {
+    "host": 'XXXX',
+    "port": 3306,
+    "user": "root",
+    # "key": r"C:\Users\saas\Desktop\id_rsa_2048.id_rsa_2048",  # 秘钥
+    "passwd": "123456",
+    "db": "testtools",
+}
+
+test = {
+    "hostname": 'XXXXX',
+    "port": 22,
+    "username": "root",
+    # "pkey": r"C:\Users\saas\Desktop\id_rsa_2048.id_rsa_2048",  # 秘钥
+    "password": "XXXX",
+}
+
+
